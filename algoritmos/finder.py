33ee6b5e4716e309291d47d8be66ab6b5317dad3
Merligus <gustavomerli18@gmail.com>
2021-07-31 01:28:39 -0300

    web finder

--- algoritmos/finder.py
+++ algoritmos/finder.py
@@ -1,9 +1,9 @@
-import argparse
-import matplotlib.pyplot as plt
-import networkx as nx
-from smacof import MDS
-import numpy as np
-import pickle
+# import argparse
+# import matplotlib.pyplot as plt
+# import networkx as nx
+# from smacof import MDS
+# import numpy as np
+# import pickle
 from collections import defaultdict
 import nltk
 import string
@@ -107,198 +107,201 @@ class ClusterFinder:
                                 word_frequence[word].append(i1)
                                 word_frequence[word].append(i1+i2+1)
         top = sorted(frequent.items(), key=lambda item: item[1], reverse=True)[:n]
+        values = []
         for word, _ in top:
+            values.append((word, f'{len(set(word_frequence[word]))/length:.0%}'))
             print(f'\t{word} {len(set(word_frequence[word]))/length:.0%}')
+        return values
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-m', action='store', dest='mode', type=str, default='union')
-parser.add_argument('-i', action='store', dest='in_name', type=str, default='-')
-parser.add_argument('-d', action='store', dest='dir', type=str, default='../data')
-parser.add_argument('-f', action='store', dest='function', type=str, default='agglomerative')
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-m', action='store', dest='mode', type=str, default='union')
+# parser.add_argument('-i', action='store', dest='in_name', type=str, default='-')
+# parser.add_argument('-d', action='store', dest='dir', type=str, default='../data')
+# parser.add_argument('-f', action='store', dest='function', type=str, default='agglomerative')
 
-parsed = parser.parse_args()
-if parsed.in_name != '-':
-    parsed.in_name = '_' + parsed.in_name
-else:
-    parsed.in_name = ''
-print(25*'*', 'ARGS', 25*'*')
-print(f'Mode: {parsed.mode}')
-print(f'In name: {parsed.in_name}')
-print(f'Directory: {parsed.dir}')
-print(f'Function: {parsed.function}')
+# parsed = parser.parse_args()
+# if parsed.in_name != '-':
+#     parsed.in_name = '_' + parsed.in_name
+# else:
+#     parsed.in_name = ''
+# print(25*'*', 'ARGS', 25*'*')
+# print(f'Mode: {parsed.mode}')
+# print(f'In name: {parsed.in_name}')
+# print(f'Directory: {parsed.dir}')
+# print(f'Function: {parsed.function}')
 
-filename = f"{parsed.dir}/graph_nao_direcionado" + parsed.mode + parsed.in_name + '.npy'
-with open(filename, "rb") as f:
-    adj_mat = np.load(f)
-distance = np.nanmin(adj_mat) + np.nanmax(adj_mat) - adj_mat
+# filename = f"{parsed.dir}/graph_nao_direcionado" + parsed.mode + parsed.in_name + '.npy'
+# with open(filename, "rb") as f:
+#     adj_mat = np.load(f)
+# distance = np.nanmin(adj_mat) + np.nanmax(adj_mat) - adj_mat
 
-filename = f"{parsed.dir}/children_{parsed.function}_" + parsed.mode + parsed.in_name
-if parsed.function == 'agglomerative': # load em python
-    with open(filename + '.npy', "rb") as f:
-        children = np.load(f)
-elif parsed.function == 'multilevel':
-    with open(filename + '.pickle', 'rb') as f:
-        children = pickle.load(f)
-else:
-    raise KeyError(f'Função não disponível.')
+# filename = f"{parsed.dir}/children_{parsed.function}_" + parsed.mode + parsed.in_name
+# if parsed.function == 'agglomerative': # load em python
+#     with open(filename + '.npy', "rb") as f:
+#         children = np.load(f)
+# elif parsed.function == 'multilevel':
+#     with open(filename + '.pickle', 'rb') as f:
+#         children = pickle.load(f)
+# else:
+#     raise KeyError(f'Função não disponível.')
 
-with open(f'{parsed.dir}/index_to_journalname_{parsed.mode}{parsed.in_name}.pickle', 'rb') as handle:
-    index_to_journalname = pickle.load(handle)
+# with open(f'{parsed.dir}/index_to_journalname_{parsed.mode}{parsed.in_name}.pickle', 'rb') as handle:
+#     index_to_journalname = pickle.load(handle)
 
-with open(f'{parsed.dir}/journals_dict{parsed.in_name}.pickle', 'rb') as handle:
-    journals = pickle.load(handle)
+# with open(f'{parsed.dir}/journals_dict{parsed.in_name}.pickle', 'rb') as handle:
+#     journals = pickle.load(handle)
 
-index_to_journal_complete_name = {}
-for v in index_to_journalname:
-    journal = index_to_journalname[v]
-    if journal not in journals:
-        print(journal)
-        continue
-    if len(journals[journal]['journal_name']) > 0:
-        index_to_journal_complete_name[v] = journal + ': ' + journals[journal]['journal_name']
-    elif 'journal_name_rough' in journals[journal]:
-        index_to_journal_complete_name[v] = journal + ': ' + journals[journal]['journal_name_rough']
-    else:
-        print(f"{journal} nome não identificado")
-        index_to_journal_complete_name[v] = journal + ': ' + journal.upper()
+# index_to_journal_complete_name = {}
+# for v in index_to_journalname:
+#     journal = index_to_journalname[v]
+#     if journal not in journals:
+#         print(journal)
+#         continue
+#     if len(journals[journal]['journal_name']) > 0:
+#         index_to_journal_complete_name[v] = journal + ': ' + journals[journal]['journal_name']
+#     elif 'journal_name_rough' in journals[journal]:
+#         index_to_journal_complete_name[v] = journal + ': ' + journals[journal]['journal_name_rough']
+#     else:
+#         print(f"{journal} nome não identificado")
+#         index_to_journal_complete_name[v] = journal + ': ' + journal.upper()
 
-with open(f'{parsed.dir}/index_to_journal_complete_name{parsed.in_name}.pickle', 'wb') as handle:
-    pickle.dump(index_to_journal_complete_name, handle, protocol=2)
+# with open(f'{parsed.dir}/index_to_journal_complete_name{parsed.in_name}.pickle', 'wb') as handle:
+#     pickle.dump(index_to_journal_complete_name, handle, protocol=2)
 
-with open(f'{parsed.dir}/index_to_journal_complete_name{parsed.in_name}.pickle', 'rb') as handle:
-    index_to_journal_complete_name = pickle.load(handle)
+# with open(f'{parsed.dir}/index_to_journal_complete_name{parsed.in_name}.pickle', 'rb') as handle:
+#     index_to_journal_complete_name = pickle.load(handle)
 
-with open(f'{parsed.dir}/nauthors{parsed.in_name}.pickle', 'rb') as handle:
-    nauthors = pickle.load(handle)
+# with open(f'{parsed.dir}/nauthors{parsed.in_name}.pickle', 'rb') as handle:
+#     nauthors = pickle.load(handle)
 
-graph_n = 1
-while True:
-    print(25*'*', 'NOVA PROCURA', 25*'*')
-    print("Siglas conferências:")
-    entrada = set(input().strip().split())
-    if len(entrada) == 0:
-        break
+# graph_n = 1
+# while True:
+#     print(25*'*', 'NOVA PROCURA', 25*'*')
+#     print("Siglas conferências:")
+#     entrada = set(input().strip().split())
+#     if len(entrada) == 0:
+#         break
 
-    in_set_conf = set()
-    s = ''
-    for key in index_to_journalname:
-        if index_to_journalname[key] in entrada:
-            in_set_conf.add(key)
-            s += index_to_journalname[key] + ' ' 
-    if len(s) == 0:
-        print(f'Nenhum identificado')
-        continue
-    print(f'Identificados: {s}')
+#     in_set_conf = set()
+#     s = ''
+#     for key in index_to_journalname:
+#         if index_to_journalname[key] in entrada:
+#             in_set_conf.add(key)
+#             s += index_to_journalname[key] + ' ' 
+#     if len(s) == 0:
+#         print(f'Nenhum identificado')
+#         continue
+#     print(f'Identificados: {s}')
 
-    n_samples = len(distance)
+#     n_samples = len(distance)
     
-    print("Procurando...")
-    labels_sets = []
-    old_cluster = in_set_conf
-    cf = ClusterFinder(children, n_samples, in_set_conf, labels_sets)
-    c, iteration = cf.find_cluster(0)
-    if iteration == len(cf.children):
-        print(f"Nenhum cluster achado")
-        continue
-    cluster = cf.labels_sets[c]
-    print(f"Primeiro cluster de tamanho {len(cluster)} juntos após {iteration} iterações")
+#     print("Procurando...")
+#     labels_sets = []
+#     old_cluster = in_set_conf
+#     cf = ClusterFinder(children, n_samples, in_set_conf, labels_sets)
+#     c, iteration = cf.find_cluster(0)
+#     if iteration == len(cf.children):
+#         print(f"Nenhum cluster achado")
+#         continue
+#     cluster = cf.labels_sets[c]
+#     print(f"Primeiro cluster de tamanho {len(cluster)} juntos após {iteration} iterações")
     
-    while True:
-        print(25*'*', 'OPÇÕES', 25*'*')
-        print("0. Sair")
-        print("1. Próxima junção")
-        print("2. Mostrar conferências do cluster")
-        print("3. Mostrar frequência das palavras do cluster")
-        print("4. Mostrar grafo")
-        entrada = input().strip()
-        if len(entrada) == 0:
-            continue
+#     while True:
+#         print(25*'*', 'OPÇÕES', 25*'*')
+#         print("0. Sair")
+#         print("1. Próxima junção")
+#         print("2. Mostrar conferências do cluster")
+#         print("3. Mostrar frequência das palavras do cluster")
+#         print("4. Mostrar grafo")
+#         entrada = input().strip()
+#         if len(entrada) == 0:
+#             continue
         
-        # opcao de sair da atual
-        if entrada[0] == '0':
-            break
-        # opcao de mostrar a proxima juncao do cluster
-        elif entrada[0] == '1':
-            old_cluster = cluster
-            c, iteration = cf.find_cluster(iteration+1)
-            if iteration < len(cf.children):
-                cluster = cf.labels_sets[c]
-                print(f"Cluster de tamanho {len(cluster)} na iteração {iteration}")
-            else:
-                print(f"Não há próxima iteração")
-        # opcao de mostrar o cluster
-        elif entrada[0] == '2':
-            print(25*'*', 'Velhos', 25*'*')
-            i = 0
-            for vi in old_cluster:
-                print(f"{i}: {index_to_journal_complete_name[vi]}")
-                i += 1
+#         # opcao de sair da atual
+#         if entrada[0] == '0':
+#             break
+#         # opcao de mostrar a proxima juncao do cluster
+#         elif entrada[0] == '1':
+#             old_cluster = cluster
+#             c, iteration = cf.find_cluster(iteration+1)
+#             if iteration < len(cf.children):
+#                 cluster = cf.labels_sets[c]
+#                 print(f"Cluster de tamanho {len(cluster)} na iteração {iteration}")
+#             else:
+#                 print(f"Não há próxima iteração")
+#         # opcao de mostrar o cluster
+#         elif entrada[0] == '2':
+#             print(25*'*', 'Velhos', 25*'*')
+#             i = 0
+#             for vi in old_cluster:
+#                 print(f"{i}: {index_to_journal_complete_name[vi]}")
+#                 i += 1
 
-            print(25*'*', 'Novos', 25*'*')
-            for vi in cluster:
-                if vi not in old_cluster:
-                    print(f"{i}: {index_to_journal_complete_name[vi]}")
-                    i += 1
-        # mostra a frequencia das palavras no cluster
-        elif entrada[0] == '3':
-            sentences = []
-            for vi in cluster:
-                sentences.append(index_to_journal_complete_name[vi].lower())
-            cf.show_top(sentences, n=10)
-        # plota o cluster em forma de grafo
-        elif entrada[0] == '4':
-            g = nx.Graph()
+#             print(25*'*', 'Novos', 25*'*')
+#             for vi in cluster:
+#                 if vi not in old_cluster:
+#                     print(f"{i}: {index_to_journal_complete_name[vi]}")
+#                     i += 1
+#         # mostra a frequencia das palavras no cluster
+#         elif entrada[0] == '3':
+#             sentences = []
+#             for vi in cluster:
+#                 sentences.append(index_to_journal_complete_name[vi].lower())
+#             cf.show_top(sentences, n=10)
+#         # plota o cluster em forma de grafo
+#         elif entrada[0] == '4':
+#             g = nx.Graph()
 
-            vertices = []
-            for vi in cluster:
-                vertices.append(vi)
+#             vertices = []
+#             for vi in cluster:
+#                 vertices.append(vi)
             
-            distance_temp = np.zeros((len(cluster), len(cluster)))
-            m = MDS(ndim=2, weight_option="d-2", itmax=10000)
+#             distance_temp = np.zeros((len(cluster), len(cluster)))
+#             m = MDS(ndim=2, weight_option="d-2", itmax=10000)
 
-            journalname = {}
-            node_size = []
-            v1 = 0
-            while v1 < len(cluster):
-                v2 = v1 + 1
-                while v2 < len(cluster):
-                    if distance[vertices[v1], vertices[v2]] > 0 and distance[vertices[v1], vertices[v2]] < np.inf:
-                        if adj_mat[vertices[v1], vertices[v2]] > 0:
-                            g.add_edge(v1, v2, weight=adj_mat[vertices[v1], vertices[v2]])
-                        distance_temp[v1, v2] = distance_temp[v2, v1] = distance[vertices[v1], vertices[v2]]
-                    v2 += 1
-                journalname[v1] = index_to_journalname[vertices[v1]]
-                node_size.append(4*np.ceil(nauthors[vertices[v1]]/len(cluster)))
-                v1 += 1
+#             journalname = {}
+#             node_size = []
+#             v1 = 0
+#             while v1 < len(cluster):
+#                 v2 = v1 + 1
+#                 while v2 < len(cluster):
+#                     if distance[vertices[v1], vertices[v2]] > 0 and distance[vertices[v1], vertices[v2]] < np.inf:
+#                         if adj_mat[vertices[v1], vertices[v2]] > 0:
+#                             g.add_edge(v1, v2, weight=adj_mat[vertices[v1], vertices[v2]])
+#                         distance_temp[v1, v2] = distance_temp[v2, v1] = distance[vertices[v1], vertices[v2]]
+#                     v2 += 1
+#                 journalname[v1] = index_to_journalname[vertices[v1]]
+#                 node_size.append(4*np.ceil(nauthors[vertices[v1]]/len(cluster)))
+#                 v1 += 1
             
-            mds_model = m.fit(distance_temp) # shape = journals x n_components
-            X_transformed = mds_model['conf']
+#             mds_model = m.fit(distance_temp) # shape = journals x n_components
+#             X_transformed = mds_model['conf']
 
-            width = nx.get_edge_attributes(g, 'weight')
-            min_w = min(width.values())
-            max_w = max(width.values())
-            for w in width:
-                width[w] = 0.5 + 4*(width[w] - min_w)/(max_w - min_w)
+#             width = nx.get_edge_attributes(g, 'weight')
+#             min_w = min(width.values())
+#             max_w = max(width.values())
+#             for w in width:
+#                 width[w] = 0.5 + 4*(width[w] - min_w)/(max_w - min_w)
             
-            edge_labels = {}
-            for v1, v2, w in g.edges.data():
-                edge_labels[(v1, v2)] = f"{adj_mat[vertices[v1], vertices[v2]]:.2f}" # w['weight']
-                # print(f'{v1}:{journalname[v1]}:{nauthors[v1]}, {v2}:{journalname[v2]}:{nauthors[v2]} = {distance[vertices[v1], vertices[v2]]}:{w["weight"]}')
+#             edge_labels = {}
+#             for v1, v2, w in g.edges.data():
+#                 edge_labels[(v1, v2)] = f"{adj_mat[vertices[v1], vertices[v2]]:.2f}" # w['weight']
+#                 # print(f'{v1}:{journalname[v1]}:{nauthors[v1]}, {v2}:{journalname[v2]}:{nauthors[v2]} = {distance[vertices[v1], vertices[v2]]}:{w["weight"]}')
 
-            fig = plt.figure(figsize=(24,24))
-            ax = fig.add_axes([0,0,1,1])
-            # pos = nx.spring_layout(g)
-            pos = {}
-            for vi in range(len(cluster)):
-                pos[vi] = X_transformed[vi]
-            # print(pos)
-            nx.draw_networkx_nodes(g, pos, ax=ax, node_size=node_size, node_color="#A8C1FB")
-            nx.draw_networkx_labels(g, pos, ax=ax, labels=journalname, font_color="#DF0000", font_size=22)
-            nx.draw_networkx_edges(g, pos, ax=ax, width=list(width.values()))
-            nx.draw_networkx_edge_labels(g, pos, ax=ax, edge_labels=edge_labels, font_size=18)
-            plt.savefig(f'{parsed.dir}/graph{graph_n}.pdf')
-            graph_n += 1
+#             fig = plt.figure(figsize=(24,24))
+#             ax = fig.add_axes([0,0,1,1])
+#             # pos = nx.spring_layout(g)
+#             pos = {}
+#             for vi in range(len(cluster)):
+#                 pos[vi] = X_transformed[vi]
+#             # print(pos)
+#             nx.draw_networkx_nodes(g, pos, ax=ax, node_size=node_size, node_color="#A8C1FB")
+#             nx.draw_networkx_labels(g, pos, ax=ax, labels=journalname, font_color="#DF0000", font_size=22)
+#             nx.draw_networkx_edges(g, pos, ax=ax, width=list(width.values()))
+#             nx.draw_networkx_edge_labels(g, pos, ax=ax, edge_labels=edge_labels, font_size=18)
+#             plt.savefig(f'{parsed.dir}/graph{graph_n}.pdf')
+#             graph_n += 1
             
-            del journalname
-            del vertices
-            del g
+#             del journalname
+#             del vertices
+#             del g
---
+++ web/web.py
@@ -0,0 +1,172 @@
+# run in clustering_subareas "python -m web.web"
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+from algoritmos.smacof import MDS
+from .data import Params
+import os
+from algoritmos.finder import ClusterFinder
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+app.secret_key = "key para session"
+
+parsed = Params('2010_only_journals', 'union', './data', 'agglomerative')
+parsed.load_files()
+
+@app.route("/search", methods=["POST", "GET"])
+def search():
+    if request.method == "POST" and "list" in request.form:
+        venues_s = set(request.form["list"].strip().split())
+
+        global parsed
+        parsed = Params(request.form["in_name"], 'union', './data', request.form["function"])
+        parsed.load_files()
+
+        if len(venues_s) == 0:
+            print(f"Nenhuma venue passada")
+            return render_template("search_venues.html")
+
+        in_set_conf = set()
+        s = ''
+        for key in parsed.index_to_journalname:
+            if parsed.index_to_journalname[key] in venues_s:
+                in_set_conf.add(key)
+                s += parsed.index_to_journalname[key] + ' '
+
+        if len(s) == 0:
+            print(f"Nenhum identificado")
+            return render_template("search_venues.html")
+
+        n_samples = len(parsed.distance)
+        
+        labels_sets = []
+        parsed.old_cluster = in_set_conf
+        parsed.cf = ClusterFinder(parsed.children, n_samples, in_set_conf, labels_sets)
+        c, parsed.iteration = parsed.cf.find_cluster(0)
+        if parsed.iteration == len(parsed.cf.children):
+            print(f"Nenhum cluster achado")
+            return render_template("search_venues.html")
+
+        parsed.cluster = parsed.cf.labels_sets[c]
+        return redirect(url_for("listar_conferencias", next="0"))
+    else:
+        return render_template("search_venues.html")
+
+@app.route("/venues<next>", methods=["POST", "GET"])
+def listar_conferencias(next):
+    if request.method == "POST" or next == "1":
+        parsed.old_cluster = parsed.cluster
+        c, parsed.iteration = parsed.cf.find_cluster(parsed.iteration+1)
+        if parsed.iteration < len(parsed.cf.children):
+            parsed.cluster = parsed.cf.labels_sets[c]
+
+    if len(parsed.cluster) > 0:
+        i = 0
+        old_cluster_l = []
+        for vi in parsed.old_cluster:
+            old_cluster_l.append((i, parsed.index_to_journal_complete_name[vi]))
+            i += 1
+
+        new_cluster = []
+        for vi in parsed.cluster:
+            if vi not in parsed.old_cluster:
+                new_cluster.append((i, parsed.index_to_journal_complete_name[vi]))
+                i += 1
+
+        return render_template("show_venues.html", new=new_cluster, old=old_cluster_l, tam_l=len(parsed.cluster))
+    else:
+        return redirect(url_for("search"))
+
+@app.route("/frequency")
+def listar_frequencia():
+    if len(parsed.cluster) > 0:
+        sentences = []
+        for vi in parsed.cluster:
+            sentences.append(parsed.index_to_journal_complete_name[vi].lower())
+        lista = parsed.cf.show_top(sentences, n=10)
+
+        return render_template("show_frequency.html", word_freq=lista)
+    else:
+        return redirect(url_for("search"))
+
+@app.route("/graph")
+def show_graph():
+    if len(parsed.cluster) > 2 and len(parsed.cluster) < 15:
+        g = nx.Graph()
+
+        vertices = []
+        for vi in parsed.cluster:
+            vertices.append(vi)
+
+        distance_temp = np.zeros((len(parsed.cluster), len(parsed.cluster)))
+        m = MDS(ndim=2, weight_option="d-2", itmax=10000)
+
+        journalname = {}
+        node_size = []
+        v1 = 0
+        while v1 < len(parsed.cluster):
+            v2 = v1 + 1
+            while v2 < len(parsed.cluster):
+                if parsed.distance[vertices[v1], vertices[v2]] > 0 and parsed.distance[vertices[v1], vertices[v2]] < np.inf:
+                    if parsed.adj_mat[vertices[v1], vertices[v2]] > 0:
+                        g.add_edge(v1, v2, weight=parsed.adj_mat[vertices[v1], vertices[v2]])
+                    distance_temp[v1, v2] = distance_temp[v2, v1] = parsed.distance[vertices[v1], vertices[v2]]
+                v2 += 1
+            journalname[v1] = parsed.index_to_journalname[vertices[v1]]
+            node_size.append(4*np.ceil(parsed.nauthors[vertices[v1]]/len(parsed.cluster)))
+            v1 += 1
+
+        mds_model = m.fit(distance_temp) # shape = journals x n_components
+        X_transformed = mds_model['conf']
+
+        width = nx.get_edge_attributes(g, 'weight')
+        min_w = min(width.values())
+        max_w = max(width.values())
+        for w in width:
+            width[w] = 0.5 + 4*(width[w] - min_w)/(max_w - min_w)
+
+        edge_labels = {}
+        for v1, v2, w in g.edges.data():
+            edge_labels[(v1, v2)] = f"{parsed.adj_mat[vertices[v1], vertices[v2]]:.2f}" # w['weight']
+            # print(f'{v1}:{journalname[v1]}:{nauthors[v1]}, {v2}:{journalname[v2]}:{nauthors[v2]} = {distance[vertices[v1], vertices[v2]]}:{w["weight"]}')
+
+        fig = plt.figure(figsize=(24,24))
+        ax = fig.add_axes([0,0,1,1])
+        # pos = nx.spring_layout(g)
+        pos = {}
+        for vi in range(len(parsed.cluster)):
+            pos[vi] = X_transformed[vi]
+        # print(pos)
+        nx.draw_networkx_nodes(g, pos, ax=ax, node_size=node_size, node_color="#A8C1FB")
+        nx.draw_networkx_labels(g, pos, ax=ax, labels=journalname, font_color="#DF0000", font_size=22)
+        nx.draw_networkx_edges(g, pos, ax=ax, width=list(width.values()))
+        nx.draw_networkx_edge_labels(g, pos, ax=ax, edge_labels=edge_labels, font_size=18)
+        filename = f"graph{np.random.random()}.png"
+        for file in os.listdir("./web/static/images/"):
+            os.remove("./web/static/images/" + file)
+        plt.savefig(f'./web/static/images/{filename}')
+
+        del journalname
+        del vertices
+        del g
+
+        # setting the list
+        i = 0
+        old_cluster_l = []
+        for vi in parsed.old_cluster:
+            old_cluster_l.append((i, parsed.index_to_journal_complete_name[vi]))
+            i += 1
+
+        new_cluster = []
+        for vi in parsed.cluster:
+            if vi not in parsed.old_cluster:
+                new_cluster.append((i, parsed.index_to_journal_complete_name[vi]))
+                i += 1
+        
+        return render_template("show_graph.html", src=filename, new=new_cluster, old=old_cluster_l, tam_l=len(parsed.cluster))
+    else:
+        return redirect(url_for("listar_conferencias", next="0"))
+
+if __name__ == "__main__":
+    app.run(debug=False)
