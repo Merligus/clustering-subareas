03b67c0c5cc51377ec6e1ee47b50004171957655
Merligus <gustavomerli18@gmail.com>
2021-08-05 18:38:20 -0300

    client session web

--- web/data.py
+++ web/data.py
@@ -2,62 +2,30 @@ import numpy as np
 import pickle
 from algoritmos.finder import ClusterFinder
 
-class Params():
-    def __init__(self, in_name, mode, dir, function):
-        
-        self.in_name = in_name
-        if self.in_name != '-':
-            self.in_name = '_' + self.in_name
-        else:
-            self.in_name = ''
-        self.mode = mode
-        self.dir = dir
-        self.function = function
+class Data:
+    def __init__(self, in_name, mode, dir):
+        self.children = {}
+        for function in ["agglomerative", "multilevel"]:
+            filename = f"{dir}/children_{function}_" + mode + in_name
+            if function == "agglomerative":
+                with open(filename + '.npy', "rb") as f:
+                    ch = np.load(f)
+                    self.children[function] = ch.tolist()
+            elif function == "multilevel":
+                with open(filename + '.pickle', 'rb') as f:
+                    self.children[function] = pickle.load(f)
+            else:
+                raise KeyError(f'Função não disponível.')
 
-        self.distance = np.zeros((2,2))
-        self.adj_mat = np.zeros((2,2))
-        self.children = [2, 2]
-        self.index_to_journalname = [2, 2]
-        self.journals = {'a': [1, 2], 'b':[3, 4]}
-        self.index_to_journal_complete_name = {'a': [1, 2], 'b':[3, 4]}
-        self.nauthors = [2, 2]
-        self.iteration = 0
-        self.old_cluster = {'a': [1, 2], 'b':[3, 4]}
-        self.cluster = {'a': [1, 2], 'b':[3, 4]}
-        self.cf = ClusterFinder(self.children, len(self.distance), set([0]), [])
-
-        print(25*'*', 'ARGS', 25*'*')
-        print(f'Mode: {self.mode}')
-        print(f'In name: {self.in_name}')
-        print(f'Directory: {self.dir}')
-        print(f'Function: {self.function}')
-    
-    def serialize(self):
-        return
-    
-    def deserialize(self):
-        return
-
-    def load_files(self):
-        filename = f"{self.dir}/graph_nao_direcionado" + self.mode + self.in_name + '.npy'
+        filename = f"{dir}/graph_nao_direcionado" + mode + in_name + '.npy'
         with open(filename, "rb") as f:
             self.adj_mat = np.load(f)
         self.distance = np.nanmin(self.adj_mat) + np.nanmax(self.adj_mat) - self.adj_mat
 
-        filename = f"{self.dir}/children_{self.function}_" + self.mode + self.in_name
-        if self.function == 'agglomerative': # load em python
-            with open(filename + '.npy', "rb") as f:
-                self.children = np.load(f)
-        elif self.function == 'multilevel':
-            with open(filename + '.pickle', 'rb') as f:
-                self.children = pickle.load(f)
-        else:
-            raise KeyError(f'Função não disponível.')
-
-        with open(f'{self.dir}/index_to_journalname_{self.mode}{self.in_name}.pickle', 'rb') as handle:
+        with open(f'{dir}/index_to_journalname_{mode}{in_name}.pickle', 'rb') as handle:
             self.index_to_journalname = pickle.load(handle)
 
-        with open(f'{self.dir}/journals_dict{self.in_name}.pickle', 'rb') as handle:
+        with open(f'{dir}/journals_dict{in_name}.pickle', 'rb') as handle:
             self.journals = pickle.load(handle)
 
         self.index_to_journal_complete_name = {}
@@ -73,17 +41,43 @@ class Params():
                 suff += " -- " + self.journals[journal]['journal_name_rough']
             self.index_to_journal_complete_name[v] = journal + ':' + suff
 
-        with open(f'{self.dir}/index_to_journal_complete_name{self.in_name}.pickle', 'wb') as handle:
+        with open(f'{dir}/index_to_journal_complete_name{in_name}.pickle', 'wb') as handle:
             pickle.dump(self.index_to_journal_complete_name, handle, protocol=2)
 
-        with open(f'{self.dir}/index_to_journal_complete_name{self.in_name}.pickle', 'rb') as handle:
+        with open(f'{dir}/index_to_journal_complete_name{in_name}.pickle', 'rb') as handle:
             self.index_to_journal_complete_name = pickle.load(handle)
 
-        with open(f'{self.dir}/nauthors{self.in_name}.pickle', 'rb') as handle:
+        with open(f'{dir}/nauthors{in_name}.pickle', 'rb') as handle:
             self.nauthors = pickle.load(handle)
+
+class Params:
+    def __init__(self, in_name, mode, dir, function, n_samples, iteration=-1, 
+                 children=[], old_cluster=[], cluster=[], in_set_conf=[0]):
         
-        self.iteration = 0
-        self.old_cluster = set()
-        self.cluster = set()
-        self.cf = ClusterFinder(self.children, len(self.distance), set([0]), [])
+        self.in_name = in_name
+        if self.in_name != '-':
+            self.in_name = '_' + self.in_name
+        else:
+            self.in_name = ''
+        self.mode = mode
+        self.dir = dir
+        self.function = function
+        
+        self.old_cluster = set(old_cluster)
+        self.cluster = set(cluster)
+        self.cf = ClusterFinder(children, n_samples, set(in_set_conf), [])
+        self.iteration = iteration
+        
+        if self.iteration >= 0:
+            _, aux_iteration = self.cf.find_cluster(0)
+            while aux_iteration != self.iteration:
+                _, aux_iteration = self.cf.find_cluster(aux_iteration+1)
+                if aux_iteration == len(self.cf.children):
+                    print(f"Nenhum cluster achado")
+
+        print(25*'*', 'ARGS', 25*'*')
+        print(f'Mode: {self.mode}')
+        print(f'In name: {self.in_name}')
+        print(f'Directory: {self.dir}')
+        print(f'Function: {self.function}')
         
